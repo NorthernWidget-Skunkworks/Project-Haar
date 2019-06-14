0d8bbcd9bc9bdf9c52a4d73ba1ddad971dc7ef3c
Bobby Schulz <schu3119@umn.edu>
2019-06-14 18:43:28 -0500

    Added ALPHA test interface
    Testing and library integration required for interface

---
+++ Haar_InterfaceDev/Haar_InterfaceDev.ino
@@ -0,0 +1,96 @@
+#include "Margay.h"
+#include <Wire.h>
+
+enum Sensor {
+	RH = 0,
+	Pressue = 1
+}
+
+#define CTRL1 0x00
+#define TEMP_PRES 0x02
+#define TEMP_RH 0x04
+#define PRES_REG 0x05
+#define RH_REG 0x09
+
+Margay Logger(Model_2v0);
+
+void setup() 
+{
+	Logger.PowerAux(OFF); 
+	delay(500); //Cycle power 
+	Logger.PowerAux(ON);  //Turn on external power
+
+	Wire.begin();
+	Serial.begin(9600); //Initalze serial for output
+}
+
+void loop() 
+{
+	//Print data
+	Serial.println("Begin Data...");
+	Serial.print("Temp, RH = "); Serial.print(GetTemp(RH)); Serial.println("°C");
+	Serial.print("Temp, Pres = "); Serial.print(GetTemp(Pressue)); Serial.println("°C");
+	Serial.print("Pressure = "); Serial.print(GetPressure()); Serial.println("mBar");
+	Serial.print("Humidity = "); Serial.print(GetRH()); Serial.println("%");
+	Serial.println("... End Data");
+	delay(1000); //Wait a bit
+}
+
+float GetPressure()   //Returns pressure [mBar]
+{
+	int32_t Val = 0; //Val for getting/calculating pressure value
+	// uint8_t Data[3] = {0}; //Storage for byte values
+	Wire.beginTransmission(ADR);
+	Wire.write(PRES_REG);
+	Wire.endTransmission();
+	Wire.requestFrom(ADR, 3);
+	while(Wire.available() < 3); //Wait for data to be ready //FIX!!!
+	for(int i = 0; i < 3; i++) {
+		// Data[i] = Wire.read(); //Read in 24 bit value
+		Val = (Val << 8) | Wire.read();
+	}
+	// Val = (Data[2] << 16) | (Data[1] << 8) | Data[0]; //Concatonate values 
+
+	if (Val & 0x800000){  //Sign extend if required 
+      Val = (0xFF000000 | Val);
+    }
+
+  	return (Val / 4096.0);
+}
+
+float GetRH()  //Returns humidity [%]
+{
+	float Val = 0; //Val for getting/calculating RH value
+	Val = GetWord(RH_REG);
+	Val = (100.0*Val)/65535.0;  //Convert to RH
+	return Val;
+}
+
+float GetTemp(Sensor Device)  //Returns temp [C], pass which sensor is desired 
+{
+	float Val = 0; //Val for getting/calculating temp value
+	if(Device == Pressue) {
+		Val = GetWord(TEMP_PRES);
+		Val = Val/100.0; //Convert to C
+	}
+
+	else if (Device == RH) {
+		Val = GetWord(TEMP_RH);
+		Val = ((Val*175.0)/65535.0) - 45; //Convert to C
+	}
+	return Val;
+}
+
+uint16_t GetWord(uint8_t Reg)  //Returns word, read from Reg position
+{
+	uint16_t Val = 0; //Val to be read from device
+	Wire.beginTransmission(ADR);
+	Wire.write(Reg);
+	Wire.endTransmission();
+
+	Wire.requestFrom(ADR, 2);  //Request word
+	while(Wire.available() < 2); //Wait //FIX! Add timeout
+	Val = Wire.read();
+	Val = Val | (Wire.read() << 8);  //Concatonate 16 bits 
+	return Val; 
+}
